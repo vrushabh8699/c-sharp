b2f8e0a62d8c33f52d1f2649ad65ca80a967740a
Vrushabh Kawale <148538012+vrushabh8699@users.noreply.github.com>
2024-02-02 20:49:05 +0530

    Is and as keyword

---
+++ is_and_as/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace is_and_as
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Console.WriteLine("enter a values");
+            //int a = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("enter b values");
+            //int b = int.Parse(Console.ReadLine());
+            //Studant(a,b);
+
+
+            ************************************
+
+            object s = "abc";
+
+            if (s is string)
+            {
+                Console.WriteLine("this is of same type");
+            }
+            else
+            {
+                Console.WriteLine("not same");
+            }
+            object s = 123;
+            string x = s as string;
+
+            Console.ReadLine();
+
+
+        }
+        
+        static void Studant(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine($"addition: {result}");
+
+
+        }
+    }
+    
+}
